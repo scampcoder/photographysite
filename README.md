998a9710e769f789e532bff65d113a6283e751db
Victoria Kee <victoriakee95@gmail.com>
2021-09-29 12:04:12 -0500

    Navbar build

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
  ## What I Learned:
  - How to use @keyframes and CSS animation for a more exciting webpage intro
 
- - 
+ - More use of using jQuery to toggle CSS classes
--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-
+//Hamburger toggle
+$(document).ready(function() {
+    $('.nav-button').click(function() {
+        $('.nav-button').toggleClass('change')
+    });
+//End of hamburger toggle
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if(position >= 200){
+            $('.nav-menu').addClass('custom-navbar');
+        } else {
+            $('.nav-menu').removeClass('custom-navbar');
+        }
+    });
+});
 
 
 
