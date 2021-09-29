6042de19de72a15c07a915215ee40bf110b7d671
Victoria Kee <victoriakee95@gmail.com>
2021-09-29 12:29:24 -0500

    Mission animation

--- script.js
+++ script.js
@@ -13,9 +13,20 @@ $(document).ready(function() {
             $('.nav-menu').removeClass('custom-navbar');
         }
     });
-});
 //End of navbar scroll styles
-
+//Mission animation
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if(position >= 650){
+            $('.camera-img').addClass('fromLeft');
+            $('.mission-text').addClass('fromRight');
+        } else {
+            $('.camera-img').removeClass('fromLeft');
+            $('.mission-text').removeClass('fromRight');
+        }
+    })
+//End of mission animation
+});
 
 
 
