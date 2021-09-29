db3e1c8f92f19f6ce2bae2430afd8ecd4c36cbea
Victoria Kee <victoriakee95@gmail.com>
2021-09-29 13:49:06 -0500

    Gallery

--- script.js
+++ script.js
@@ -26,6 +26,22 @@ $(document).ready(function() {
         }
     })
 //End of mission animation
+//Gallery filter
+    $('.gallery-list-item').click(function() {
+        let value = $(this).attr('data-filter');
+        if(value === 'all') {
+            $('.filter').show(300);
+        } else {
+            $('.filter').not('.' + value).hide(300);
+            $('.filter').filter('.' + value).show(300)
+        }
+    });
+//End of gallery filter
+//Active gallery item
+    $('.gallery-list-item').click(function() {
+        $(this).addClass('active-item').siblings().removeClass('active-item')
+    })
+//End of active gallery item
 });
 
 
